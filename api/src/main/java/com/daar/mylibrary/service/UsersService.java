ec7f618eb46e78630dc541796cd0aedc1c37b81f
alexandre-em <alexandre.em@monkvision.ai>
2022-02-06 00:30:00 +0100

    Add docs from UsersService

--- api/src/main/java/com/daar/mylibrary/service/UsersService.java
+++ api/src/main/java/com/daar/mylibrary/service/UsersService.java
@@ -30,6 +30,13 @@ public class UsersService {
         return userRepository.findByUserIdAndDeletedAtIsNull(id);
     }
 
+    /**
+     * Get the suggestion list of a user based on read books and previous searches
+     * @param id    user id
+     * @return a list of suggested books
+     * @see Books
+     * @throws NotFoundException User with this id does not exist
+     */
     public List<Books> suggestionBooks(String id) throws NotFoundException {
         User user = findUser(id);
         if (user == null) throw new NotFoundException("user not found");
@@ -43,21 +50,45 @@ public class UsersService {
                 .map(BooksCont::getId).getContent(), PageRequest.of(0, n))
                 .getContent();
         authorB.addAll(history);
+
         return authorB;
     }
 
+    /**
+     * Get all user's previously read books
+     * @param id    user id
+     * @param page  current page
+     * @param limit max number of books on a page
+     * @return a list of read books
+     * @see Books
+     * @throws NotFoundException the user does not exist
+     */
     public List<Books> getReadBooks(String id, int page, int limit) throws NotFoundException {
         User user = findUser(id);
         if (user == null) throw new NotFoundException("user not found");
         return userRepository.findUserReadBooks(id, PageRequest.of(page, limit));
     }
 
+    /**
+     * Add a user on the db
+     * @param id    user id
+     * @see User
+     * @return added user
+     */
     public User addUser(String id) {
         User user = findUser(id);
         if (user != null) return user;
         return userRepository.save(new User(id));
     }
 
+    /**
+     * Add keywords to user's history search
+     * @param uuid      user id
+     * @param keywords  search keywords
+     * @return updated user
+     * @see User
+     * @throws NotFoundException user is not on the db
+     */
     public User addKeyword(String uuid, List<String> keywords) throws NotFoundException {
         User user = findUser(uuid);
         if (user == null) throw new NotFoundException("user not found");
@@ -72,6 +103,14 @@ public class UsersService {
         return userRepository.save(user);
     }
 
+    /**
+     * Add books to user's read books list
+     * @param id        user id
+     * @param bookId    books id
+     * @return updated user
+     * @see User
+     * @throws NotFoundException user or book is not on the db
+     */
     public User addBooksRead(String id, String bookId) throws NotFoundException {
         Books book = booksRepository.findBooksByBookId(bookId);
         User user = findUser(id);
@@ -81,6 +120,12 @@ public class UsersService {
         return userRepository.save(user);
     }
 
+    /**
+     * Soft delete a user
+     * @param id    user id
+     * @return the removed user
+     * @throws NotFoundException user already removed or not present on the db
+     */
     public User deleteUser(String id) throws NotFoundException {
         User user = findUser(id);
         if (user == null) throw new NotFoundException("user not found");
