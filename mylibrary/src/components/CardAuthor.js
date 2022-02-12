a9886abb116b4d69efb1dedfdc8e88fdb120c127
alexandre-em <alexandre.em@monkvision.ai>
2022-02-12 01:23:04 +0100

    Replace pagination + clean code Remove unnecessary calls

--- mylibrary/src/components/CardAuthor.js
+++ mylibrary/src/components/CardAuthor.js
@@ -1,76 +1,98 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, Dimensions, Text, View  } from 'react-native';
-import { Avatar, Button, Card, Title, Paragraph, Dialog, Portal } from 'react-native-paper';
-import noImage from 'assets/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg';
-import { getContentByID, getId } from 'services';
-import jwt_decode from "jwt-decode";
-import { useSelector } from 'react-redux';
-import { bookRead } from 'services/users';
-import { getAuthorDetails } from 'services/authors';
+import {
+  ScrollView, Dimensions, Text, View,
+} from 'react-native';
+import {
+  Avatar,
+  Button, Card, Dialog, Divider, Portal,
+} from 'react-native-paper';
+import propsType from 'prop-types';
 
-export default function CardAuthor(props) {
-    const { name, id } = props.item
-    const auth = useSelector((state) => state.auth);
+import { getAuthorDetails } from 'services';
 
-    const [authorsBooks, setAuthorsBooks] = useState([]);
+import profil1 from 'assets/profil1.png';
+import profil2 from 'assets/profil2.png';
+import profil3 from 'assets/profil3.png';
+import profil4 from 'assets/profil4.png';
 
-    const windowHeight = Dimensions.get('window').height;
-    const windowWidth = Dimensions.get('window').width;
+export default function CardAuthor({ item }) {
+  const { name, id } = item;
+  const windowHeight = Dimensions.get('window').height;
+  const avatarList = [profil1, profil2, profil3, profil4];
 
-    const [visible, setVisible] = useState(false);
+  const [authorsBooks, setAuthorsBooks] = useState([]);
+  const [visible, setVisible] = useState(false);
 
-    const manageDialog = () => setVisible(prevState => !prevState);
+  const manageDialog = () => setVisible((prevState) => !prevState);
 
-    const authorDetails = () =>{
-        getAuthorDetails(id)
-        .then((res)=>{
-            setAuthorsBooks(res.data.books);
-          })
-        .catch((err)=>{
-            console.log(err);
+  useEffect(() => {
+    if (visible) {
+      getAuthorDetails(id)
+        .then((res) => {
+          setAuthorsBooks(res.data.books);
         })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-    
-    useEffect(() => {
-        authorDetails();
-    }, []);
+  }, [visible]);
 
-   
+  return (
+    <>
+      <Card key={id} style={{ width: 300, margin: 10 }}>
+        <Card.Content style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Avatar.Image size={150} source={avatarList[name.charCodeAt(0) % avatarList.length]} />
+        </Card.Content>
+        <Card.Title style={{ textOverflow: 'ellipsis', overflow: 'hidden' }} title={name} />
+        <Card.Actions>
+          <Button onPress={() => {
+            manageDialog();
+          }}
+          >
+            View more
+          </Button>
+        </Card.Actions>
+      </Card>
 
+      <Portal>
+        <Dialog visible={visible} onDismiss={manageDialog} style={{ height: windowHeight }}>
+          <Dialog.Title>Author&apos;s books</Dialog.Title>
+          <Dialog.ScrollArea>
+            <ScrollView>
+              {authorsBooks.map((book) => (
+                <View key={book.id}>
+                  <Dialog.Title>{book.title}</Dialog.Title>
+                  <Dialog.Content>
+                    <Text>
+                      Date de publication:
+                      {book.year}
+                    </Text>
+                  </Dialog.Content>
+                  <Divider />
+                </View>
+              ))}
+            </ScrollView>
+          </Dialog.ScrollArea>
+          <Dialog.Actions>
+            <Button onPress={() => manageDialog()}>Done</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </>
 
-    
-
-    return (
-        <>
-            <Card key={id} style={{width:300, margin:10}}>
-                
-                <Card.Title style={{textOverflow: 'ellipsis',overflow: 'hidden' }} title={name} />
-                <Card.Actions>
-                    <Button onPress={() => {
-                            manageDialog();
-                    }}>View more</Button>
-                </Card.Actions>
-            </Card>
-
-            <Portal>
-                <Dialog visible={visible} onDismiss={manageDialog} style={{height:windowHeight}}>
-                <Dialog.ScrollArea>
-                    <ScrollView>
-                    {authorsBooks.map(book =><View key={book.id}>
-                        <Dialog.Title>{book.title}</Dialog.Title>
-                        <Text>Date de publication: {book.year}</Text> 
-                        
-                    </View>)}
-                        
-                    </ScrollView>
-                </Dialog.ScrollArea>
-                    
-                    <Dialog.Actions>
-                        <Button onPress={() => manageDialog()}>Done</Button>
-                    </Dialog.Actions>
-                </Dialog>
-                </Portal>
-        </>
-        
-    );
+  );
 }
+
+CardAuthor.propTypes = {
+  item: propsType.shape({
+    name: propsType.string,
+    id: propsType.string,
+  }),
+};
+
+CardAuthor.defaultProps = {
+  item: {
+    name: '',
+    id: '',
+  },
+};
--- mylibrary/src/screens/Author.js
+++ mylibrary/src/screens/Author.js
@@ -1,96 +1,89 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
-import { getAllAuthor } from 'services/authors';
-import { Button } from 'react-native-paper';
-import CardAuthor from 'components/CardAuthor';
-export default function Author() {
+import {
+  View, ScrollView, StyleSheet,
+} from 'react-native';
 
-    
-    const [authorsQuery, setAuthorsQuery] = useState([]);
-    const [page, setPage] = useState(0);
-    
-    const [totalPage, setTotalPage] = useState(0);
-    
-    useEffect(() => {
-      getAllAuthor(0)
-      .then((res)=>{
-          setPage(0)
-          setAuthorsQuery(res.data.data);
-          setTotalPage(res.data.totalPage)
-      })
-      .catch((err)=>{
-          console.log(err);
-      })
-    },[]);
-
-    
-    const changePage = (nb) =>{
-        if((nb+page < totalPage) && (page+nb >=0)){
-            getAllAuthor(nb+page).then((res)=>{
-            setPage(nb+page);
-            setAuthorsQuery(res.data.data);
-          })
-        }
-    }
-    
-
-    
-
-
-    return (
-        <ScrollView>
-          <View style={styles.containerCard}>
-          {authorsQuery.map(element =>
-            <CardAuthor key={element.id} item = {element}></CardAuthor>)
-            
-          }
-          </View>
-            <View style={styles.footerScroll}>
-                {page-1 >=0?<Button style={styles.buttonType} mode="contained" onPress={() => changePage(-1)}>{"<"}</Button>:<></>}
-                <Text style={{marginTop:8}}>{page}</Text>
-                {1+page < totalPage?<Button style={styles.buttonType} mode="contained" onPress={() => changePage(1)}>{">"}</Button>:<></>}
-            </View>
-            
-        </ScrollView>
-    );
-}
+import { CardAuthor } from 'components';
+import { getAllAuthor } from 'services';
+import { DataTable } from 'react-native-paper';
 
 const styles = StyleSheet.create({
-    flatlist: {
-      flexDirection: 'column',
-    },
-    buttonType : {
-      width:"5%",
-      marginRight:30,
-    },
-    containerGrid: {
-      display: 'flex',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-    },
-    itemGrid: {
-      flexGrow: 1,
-    },
-    containerCard :{
-      display : 'flex',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-      
-    },
-    footerScroll :{
-      display:'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      marginTop : 10,
-      marginBottom : 10,
-      marginLeft : 10,
-      marginRight : 10,
-    },
-    buttonType : {
-      width:"5%",
-      marginRight:30,
-      marginLeft: 30,
-    },
-  });
+  flatlist: {
+    flexDirection: 'column',
+  },
+  buttonType: {
+    width: '5%',
+    marginRight: 30,
+    marginLeft: 30,
+  },
+  containerGrid: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  itemGrid: {
+    flexGrow: 1,
+  },
+  containerCard: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+
+  },
+  footerScroll: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+});
+
+const numberOfItemsPerPageList = [10, 20, 50, 100];
+
+export default function Author() {
+  const [authorsQuery, setAuthorsQuery] = useState([]);
+  const [page, setPage] = useState(0);
+
+  const [totalPage, setTotalPage] = useState(0);
+  const [totalAuthors, setTotalAuthors] = useState(0);
+  const [authorsPerPage, setAuthorsPerPage] = useState(10);
+
+  useEffect(() => {
+    getAllAuthor(page, authorsPerPage)
+      .then((res) => {
+        setAuthorsQuery(res.data.data);
+        setTotalPage(res.data.totalPage);
+        setTotalAuthors(res.data.totalElement);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [page, authorsPerPage]);
+
+  return (
+    <ScrollView>
+      <View style={styles.containerCard}>
+        {authorsQuery.map((element) => <CardAuthor key={element.id} item={element} />)}
+      </View>
+      <View style={styles.footerScroll}>
+        <DataTable.Pagination
+          page={page}
+          numberOfPages={totalPage}
+          onPageChange={(p) => setPage(p)}
+          label={`${page * authorsPerPage} of ${totalAuthors}`}
+          showFastPaginationControls
+          numberOfItemsPerPageList={numberOfItemsPerPageList}
+          numberOfItemsPerPage={authorsPerPage}
+          onItemsPerPageChange={setAuthorsPerPage}
+          selectPageDropdownLabel="Books per page"
+        />
+      </View>
+
+    </ScrollView>
+  );
+}
