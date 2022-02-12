ae382e3e61f63c51c4fdada602b45117f93415f2
alexandre-em <alexandre.em@monkvision.ai>
2022-02-12 01:24:49 +0100

    Replace pagination + picker Remove RN picker library

--- mylibrary/src/screens/Search.js
+++ mylibrary/src/screens/Search.js
@@ -1,139 +1,147 @@
-import React, { useState } from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Searchbar, Button } from 'react-native-paper';
+import {
+  View, ScrollView, StyleSheet,
+} from 'react-native';
+import {
+  Searchbar, Button, Menu, Chip, DataTable,
+} from 'react-native-paper';
+
 import { publicSearch, userSearch } from 'services';
-import CardBook from 'components/CardBook';
-import { Picker } from '@react-native-picker/picker';
-
-export default function Search() {
-
-    const auth = useSelector((state) => state.auth);
-    const [searchQuery, setSearchQuery] = useState('');
-    const [publicBooks, setPublicBooks] = useState([]);
-    const [page, setPage] = useState(0);
-    const [totalPage, setTotalPage] = useState(0);
-    const [selectedValue, setSelectedValue] = useState("DEFAULT");
-    const onChangeSearch = query => setSearchQuery(query);
-
-    const changePage = (nb) =>{
-        if((nb+page < totalPage) && (page+nb >=0)){
-            if(auth.isAuthenticated){
-                userSearch(searchQuery, auth.accessToken, selectedValue, nb+page)
-                .then((res)=>{
-                    setPage(nb+page);
-                    setPublicBooks(res.data.data);
-                }).catch((err)=>(console.log(err)))
-            }
-            else{
-                publicSearch(searchQuery, selectedValue, nb+page)
-                .then((res)=>{
-                    setPage(nb+page);
-                    setPublicBooks(res.data.data);
-                }).catch((err)=>(console.log(err)))
-            }
-        }
-        
-    }
-
-    const searchBook = () =>{
-        setPage(0)
-        if(auth.isAuthenticated){
-            userSearch(searchQuery, auth.accessToken, selectedValue, 0)
-            .then((res)=>{
-                
-                setPublicBooks(res.data.data);
-                setTotalPage(res.data.totalPage)
-            })
-            .catch((err)=>{
-                console.log(err);
-            });
-        }
-        else{
-            publicSearch(searchQuery, selectedValue, 0)
-            .then((res)=>{
-                setPublicBooks(res.data.data);
-                setTotalPage(res.data.totalPage)
-            })
-            .catch((err)=>{
-                console.log(err);
-            });
-        }
-    }
-    
-    return (
-        <ScrollView>
-            
-            <Searchbar
-                placeholder="Search"
-                onChangeText={onChangeSearch}
-                value={searchQuery}
-                onIconPress={()=>searchBook()}
-                onSubmitEditing={()=>searchBook()}
-                style={{margin:10}}
-            />
-            <View>
-                <Text style={{marginLeft:10}}>Option de recherche</Text>
-                <Picker
-                    selectedValue={selectedValue}
-                    style={{ height: 50, width: 150 }}
-                    onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-                    style={{marginLeft:10}}
-                >
-                    <Picker.Item label="Default" value="DEFAULT" />
-                    <Picker.Item label="Title" value="TITLE" />
-                    <Picker.Item label="Author" value="AUTHOR" />
-                    <Picker.Item label="Year" value="YEAR" />
-                    <Picker.Item label="Regex" value="REGEX" />
-                </Picker>
-            </View>
-            
-
-            <View style={styles.containerCard}>
-                {publicBooks.map(item=>
-                    <CardBook key={item.id} item = {item}></CardBook>
-                )}
-            </View>
-            <View style={styles.footerScroll}>
-                {page-1 >=0?<Button style={styles.buttonType} mode="contained" onPress={() => changePage(-1)}>{"<"}</Button>:<></>}
-                <Text style={{marginTop:8}}>{page}</Text>
-                {1+page < totalPage?<Button style={styles.buttonType} mode="contained" onPress={() => changePage(1)}>{">"}</Button>:<></>}
-            </View>
-
-        </ScrollView>
-    );
-}
+import { CardBook } from 'components';
 
 const styles = StyleSheet.create({
-    flatlist: {
-      flexDirection: 'column',
-    },
-    buttonType : {
-        width:"5%",
-        marginRight:30,
-        marginLeft: 30,
-      },
-    containerGrid: {
-      display: 'flex',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-    },
-    itemGrid: {
-      flexGrow: 1,
-    },
-    containerCard :{
-      display : 'flex',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-    },
-    footerScroll :{
-        display:'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop : 10,
-        marginBottom : 10,
-        marginLeft : 10,
-        marginRight : 10,
-      },
-  });
+  flatlist: {
+    flexDirection: 'column',
+  },
+  buttonType: {
+    width: '5%',
+    marginRight: 30,
+    marginLeft: 30,
+  },
+  containerGrid: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  itemGrid: {
+    flexGrow: 1,
+  },
+  containerCard: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  footerScroll: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+});
+
+const numberOfItemsPerPageList = [10, 20, 50, 100];
+
+export default function Search() {
+  const auth = useSelector((state) => state.auth);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [publicBooks, setPublicBooks] = useState([]);
+  const [page, setPage] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
+  const [booksPerPage, setBooksPerPage] = useState(10);
+  const [totalBooks, setTotalBooks] = useState(0);
+  const [showMenu, setShowMenu] = useState(false);
+  const [selectedValue, setSelectedValue] = useState('DEFAULT');
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  const searchBook = useCallback((p, bpp) => {
+    if (auth.isAuthenticated) {
+      userSearch(searchQuery, auth.accessToken, selectedValue, p, bpp)
+        .then((res) => {
+          setPublicBooks(res.data.data);
+          setTotalPage(res.data.totalPage);
+          setTotalBooks(res.data.totalElement);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      publicSearch(searchQuery, selectedValue, p, bpp)
+        .then((res) => {
+          setPublicBooks(res.data.data);
+          setTotalPage(res.data.totalPage);
+          setTotalBooks(res.data.totalElement);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [auth, searchQuery, selectedValue]);
+
+  useEffect(() => {
+    searchBook(page, booksPerPage);
+  }, [page, booksPerPage]);
+
+  const handleSelect = (value) => {
+    setSelectedValue(value);
+    setShowMenu(false);
+  };
+
+  return (
+    <ScrollView>
+
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        onIconPress={() => searchBook(0, 10)}
+        onSubmitEditing={() => searchBook(0, 10)}
+        style={{ margin: 10 }}
+      />
+      <View>
+        <Menu
+          visible={showMenu}
+          onDismiss={() => setShowMenu(false)}
+          anchor={(
+            <Button
+              mode="outlined"
+              style={{ margin: 10 }}
+              onPress={() => setShowMenu(true)}
+            >
+              Filtres
+            </Button>
+)}
+        >
+          <Menu.Item onPress={() => handleSelect('DEFAULT')} title="DEFAULT" />
+          <Menu.Item onPress={() => handleSelect('TITLE')} title="TITLE" />
+          <Menu.Item onPress={() => handleSelect('AUTHOR')} title="AUTHOR" />
+          <Menu.Item onPress={() => handleSelect('YEAR')} title="YEAR" />
+          <Menu.Item onPress={() => handleSelect('REGEX')} title="REGEX" />
+        </Menu>
+        <Chip style={{ margin: 10, width: 'min-content' }}>{selectedValue}</Chip>
+      </View>
+
+      <View style={styles.containerCard}>
+        {publicBooks.map((item) => <CardBook key={item.id} item={item} />)}
+      </View>
+      <View style={styles.footerScroll}>
+        <DataTable.Pagination
+          page={page}
+          numberOfPages={totalPage}
+          onPageChange={(p) => setPage(p)}
+          label={`${page * booksPerPage} of ${totalBooks}`}
+          showFastPaginationControls
+          numberOfItemsPerPageList={numberOfItemsPerPageList}
+          numberOfItemsPerPage={booksPerPage}
+          onItemsPerPageChange={setBooksPerPage}
+          selectPageDropdownLabel="Books per page"
+        />
+      </View>
+
+    </ScrollView>
+  );
+}
